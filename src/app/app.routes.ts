dbc3946d750b2bbadbd875accd07da9990fc780a
evve-a18 <evelin.angel143@gmail.com>
2024-12-06 23:37:28 -0600

    cambios finales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RickandmortyPageComponent } from './pages/rickandmorty-page/rickandmorty-page.component';
+import { RickMortyEpisodesComponent } from './pages/rick-morty-episodes/rick-morty-episodes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path:'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path:'characters',
+        component: RickandmortyPageComponent
+    },
+    {
+        path:'episodes',
+        component:RickMortyEpisodesComponent
+    }
+];
